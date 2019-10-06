999e257a473203e24750e15521c05a6065d39ecb
earthctzn <earthctzn1@gmail.com>
2019-10-06 19:00:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#destroy', as: 'logout'
+  resources :users, only: [:new, :create, :show]
 end
